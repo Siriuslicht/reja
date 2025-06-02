e00206b58df8de2326c3b60a29df49b74510f67e
Siriuslicht <laurairlean@gmail.com>
2025-06-02 16:56:06 +0500

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ console.log(`Web Serverni boshlash`);
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+   if(err){
+      console.log("ERROR", err);
+   }  else {
+      user = JSON.parse(data);  
+   }
+})
 
 // Expressning 4 ta bo'limi
-// 1: Entry codes 
+// 1: Entry codes  
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -17,15 +27,20 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
-app.get("/hello", function(req, res){
-   res.end(`<h1 style="background-color: red"> Hello World! by Stein</h1>`);
-
+app.post("/create-item", (req, res) => {
+   console.log(req.body); 
+   res.json({test: "success"});
 });
 
-app.get("/gift", function(req, res){
-   res.end(`<h1> GIFT page by Stein</h1>`);
+app.get('/author', (req, res) => {
+   res.render("author", {user: user});
+})
 
+app.get('/', function(req, res) {
+      res.render("harid");
 });
+
+
 
 const server = http.createServer(app);
 let PORT = 3000;
